3989f0c9ba6ba023c888201fa3e6caeacddb39d3
Sidharth Jain <imsidharthj@gmail.com>
2024-05-01 13:43:58 +0530

    modify code

--- nasted_list/nasted.py
+++ nasted_list/nasted.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         records.append([name, score])
-    scores = sorted(score for name, score in records)
-    second_lowest_score = scores[1]
+    scores = sorted(set(score for name, score in records))
+    second_lowest_score = scores[1] if len(scores) > 1 else scores[0]
     students_with_second_lowest = [name for name, score in records if score == second_lowest_score]
     students_with_second_lowest.sort()
     for student in students_with_second_lowest:
