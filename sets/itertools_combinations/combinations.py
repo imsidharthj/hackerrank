dd1fcf34997c7897fe7710c3592e2d96a4d45c90
Sidharth Jain <imsidharthj@gmail.com>
2024-05-16 21:12:02 +0530

    itertools combination function

---
+++ sets/itertools_combinations/combinations.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+s,k = input().split()
+k = int(k)
+for i in range(1, k+1):
+    for j in combinations(sorted(s), i):
+        print("".join(j))
+# print(list(combinations(sored(s), k)))
